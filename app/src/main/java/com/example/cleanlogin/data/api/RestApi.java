25299fe40b6030cbec1b0d3639d01fc4b157971c
mona-baharlou <mona.baharlou755@gmail.com>
2024-04-26 16:05:56 +0330

    data & domain updated for repository

--- app/src/main/java/com/example/cleanlogin/data/api/RestApi.java
+++ app/src/main/java/com/example/cleanlogin/data/api/RestApi.java
@@ -1,4 +1,13 @@
-package com.example.cleanlogin.data;
+package com.example.cleanlogin.data.api;
 
-public class RestApi {
+import com.example.cleanlogin.data.model.Root;
+
+import io.reactivex.Observable;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface RestApi {
+
+    @GET("file/d/1g6zfDMcOeWYHPE6NtTL0LQsdA9W_rJF3/view?usp=sharing\n")
+    Observable<Root> login(@Query("username") String username, @Query("password") String password);
 }
